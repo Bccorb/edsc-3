f573cb0b17b4fa6648cceb87df442b6de7051a5a
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-12-22 20:10:44 -0500

    switch to blue marble

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import './App.css'
 import Globe from 'react-globe.gl';
 
 function App() {
+  // TODO pull this map down locally and have it use that we may have to branch react-globe.gl
+  // TODO fix the window h and w to fit into the page exactly without extra room
   return (
     <>
-       <Globe/>
+    <div>
+    <Globe
+       globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+       />
+    </div>
     </>
   )
 }
