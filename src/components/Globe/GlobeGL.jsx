d9a8e69915220074be1a7fda42d25e6748acf747
Brandon Corbett <bccorb1000@gmail.com>
2024-01-02 15:53:22 -0500

    Updated with volcano data.

--- src/components/Globe/GlobeGL.jsx
+++ src/components/Globe/GlobeGL.jsx
@@ -1,35 +1,56 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import newZealand from '../../assets/geoJson/newZealandSouthIsland.json';
-
+import volcanos from '../../assets/geoJson/volcanos';
 import Globe from "react-globe.gl";
 
 import "./GlobeGL.css";
 
-function GlobeGL() {
+function GlobeGL(props) {
   const globalEl = useRef(null);
+  const globeType = props.globeType || "points";
+
+  const [points, setPoints] = useState([]);
+
+  const centerOnLocation  = (lat, lng) => {
+    console.log("moving the globe", lat, lng)
+    globalEl.current.pointOfView({ lat: lat, lng: lng, altitude: 0.6 }, 4000)
+  }
 
   // Leaving commented out for now, because the rotations causes alot of rerenders.
-  // useEffect(() => {
-  //   // Make the globe automatically rotate.
-  //   if (globalEl !== null) {
-  //     globalEl.current.controls().autoRotate = true;
-  //     globalEl.current.controls().autoRotateSpeed = 0.1;
-  //   }
-  // }, []);
+  useEffect(() => {
+    // Make the globe automatically rotate.
+    // if (globalEl !== null) {
+    //   globalEl.current.controls().autoRotate = true;
+    //   globalEl.current.controls().autoRotateSpeed = 0.1;
+    // }
+
+    setPoints(volcanos)
+  }, []);
 
   // TODO pull this map down locally and have it use that we may have to branch react-globe.gl
-  return (
+  if (globeType === "points") {
+    return (
     <div className="globe">
       <Globe
         ref={globalEl}
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-        polygonsData={newZealand.features}
-        polygonCapColor={() => '#FFA500'}
+        hexBinPointsData={points}
+        hexAltitude={.01}
+        hexMargin={0}
+        hexTopCurvatureResolution={2}
+        hexBinResolution={3}
+        hexSideColor={() => 'pink'}
+        hexTopColor={() => 'red'}
+        hexBinMerge={false}
+        enablePointerInteraction={true}
+        onHexClick={(d) => { 
+          centerOnLocation(d.points[0].lat, d.points[0].lng)}}
       />
     </div>
   );
+}
 }
 
 export default GlobeGL;
